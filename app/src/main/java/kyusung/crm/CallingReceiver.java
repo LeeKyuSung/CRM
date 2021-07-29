d8c873ee86a1a24b497a4b3b1fffe831f74a41a5
LeeKyuSung <kslksks@kaist.ac.kr>
2021-07-29 23:16:48 +0900

    기능 개발 완료 - git ignore 수정 - data 폴더 수정. res/raw 폴더로 이동. - 패키지 구조 변경 - dependency - gson 추가 (필요없음. 귀찮아서 그냥 냅둠) - dependecny - notification을 위한 support-compat - 기능 개발 완료 (파일 읽어서 고객정보 들고 있고, 전화오면 고객정보 확인 후 해당 고객 주문 정보 파일 확인. 해당 파일 열어서 알림 string 생성. notification 발생)

---
+++ app/src/main/java/kyusung/crm/CallingReceiver.java
@@ -0,0 +1,140 @@
+package kyusung.crm;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import java.io.InputStream;
+
+import kyusung.crm.pojo.Customer;
+import kyusung.crm.pojo.Order;
+
+public class CallingReceiver extends BroadcastReceiver {
+
+    public static final String TAG = "PHONE STATE";
+    private static String mLastState;
+
+    private final Handler mHandler = new Handler(Looper.getMainLooper());
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        Log.d(TAG, "===================전화 수신========");
+        Log.d(TAG,"onReceive()");
+        System.out.println("[IncomingCallBroadcastReceiver][onReceive]");
+
+        String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+
+        /*if (state.equals(mLastState)) {
+            return;
+
+        } else {
+            mLastState = state;
+        }*/
+
+        if (TelephonyManager.EXTRA_STATE_RINGING.equals(state)) {
+            System.out.println("전화 울리는 중");
+
+            TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Service.TELEPHONY_SERVICE);
+            String phoneNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+
+            if (phoneNumber == null ) {
+                Log.i("call-state", " : NULL");
+            } else {
+                Log.i("phone number : ", phoneNumber);
+
+                Customer target = null;
+                boolean isFound = false;
+                for (Customer customer : MainActivity.customers) {
+                    if (customer.isTel(phoneNumber)) {
+                        System.out.println("phone number Exist!!!!");
+                        target = customer;
+                        isFound = true;
+                        break;
+                    }
+                }
+                if (isFound) {
+                    System.out.println("found customer id : " + target.getId());
+                    showCustomer(context, target);
+                } else {
+                    System.out.println("phone number not found");
+                }
+            }
+        }
+    }
+
+    public void showCustomer(final Context context, Customer customer) {
+
+        // 주문 목록 검색
+        String orderListString = "";
+
+        String filename = "user" + customer.getId();
+        String str = "";
+        boolean haveData = true;
+        try {
+            InputStream fis = context.getResources().openRawResource(context.getResources().getIdentifier(filename,"raw", context.getPackageName()));
+            byte[] data = new byte[fis.available()];
+            while(fis.read(data)!=-1) {
+                ;
+            }
+            str = new String(data);
+        } catch (Exception e) {
+            System.out.println("No order data");
+            haveData = false;
+        }
+        if (haveData) {
+            String[] inputList = str.split("\n");
+            try {
+                // TODO 최근 주문건 골라내는 로직 필요
+                for (int i=inputList.length-4; i<inputList.length; i++) {
+                    Order order = new Order(inputList[i]);
+                    String menu = order.getMenu();
+                    String price = order.getPrice();
+                    String date = order.getDate();
+                    if (!"".equals(menu)) {
+                        orderListString += menu + " ";
+                    }
+                    if (!"".equals(price)) {
+                        orderListString += price + " ";
+                    }
+                    if (!"".equals(date)) {
+                        orderListString += "("+date +")";
+                    }
+                    orderListString += "\n";
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        // 알림 띄우기
+        String tmp = customer.getNotificationStr();
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default")
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle(tmp)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(tmp + "\n" + orderListString));
+
+        // 알림 표시
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.createNotificationChannel(new NotificationChannel("default", "기본 채널", NotificationManager.IMPORTANCE_DEFAULT));
+        }
+
+        // id값은
+        // 정의해야하는 각 알림의 고유한 int값
+        notificationManager.notify(1, builder.build());
+    }
+}
---
+++ app/src/main/java/kyusung/crm/MainActivity.java
@@ -0,0 +1,70 @@
+package kyusung.crm;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+
+import java.io.InputStream;
+import java.util.HashSet;
+
+import kyusung.crm.pojo.Customer;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String location = "raw/customers";
+    public static HashSet<Customer> customers;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main2);
+        // TODO 검색 창 만들기
+
+        requestPermission();
+
+        // 파일 읽어오기
+        getCustomers();
+    }
+
+    private void requestPermission() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_CALL_LOG, Manifest.permission.READ_PHONE_STATE},1);
+        }
+    }
+
+    private void getCustomers() {
+        customers = new HashSet<>();
+
+        String str = "";
+        try {
+            InputStream fis = getResources().openRawResource(R.raw.customers);
+            byte[] data = new byte[fis.available()];
+            while(fis.read(data)!=-1) {
+                ;
+            }
+            str = new String(data);
+        } catch (Exception e) {
+            str = "failed read";
+            e.printStackTrace();
+        }
+
+        System.out.println("==============================asdfasdfafds");
+        String[] inputList = str.split("\n");
+        try {
+            for (int i=0; i<inputList.length; i++) {
+                Customer customer = new Customer(inputList[i]);
+                customers.add(customer);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
